9407ace1b38dc2d18611d40d4cab8fb52b5c2c02
Songsangsung <21700383@handong.ac.kr>
2023-08-30 21:06:38 +0900

    Changed to get actual value from KeyValue

--- src/main/java/egovframework/ocr/sample/web/Keys.java
+++ src/main/java/egovframework/ocr/sample/web/Keys.java
@@ -11,7 +11,7 @@ package egovframework.ocr.sample.web;
 * == 개정이력(Modification Information) ==
 * 수정 내용
 * 2023.08 정은총 최초생성
-* 2023.08 송창우 GPT_KEY 함수를 private 지정, GPT_KEY 값을 불러오기 위한 getGptKey 추가
+* 2023.08 송창우 GPT_KEY 함수를 private 지정, GPT_KEY 값을 불러오기 위한 getGptKey 추가 
 */
 public class Keys {
     /*Key*/
