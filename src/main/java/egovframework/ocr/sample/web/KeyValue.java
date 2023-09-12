5c2ebfd036b20f1f307d2615da61abf7b538485e
JungEunchong <21900673@handong.ac.kr>
2023-09-12 19:05:17 +0900

    backup before pull

---
+++ src/main/java/egovframework/ocr/sample/web/KeyValue.java
@@ -0,0 +1,7 @@
+package egovframework.ocr.sample.web;
+
+public class KeyValue {
+	public static String gptKey = "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";
+	
+	public static String uploadDir = "C:/Users/JEC/eclipse-workspace/ocr/src/main/java/saveImage";
+}
