d6173e14790d95828607b0d6212dbeeeff99d0b9
Songsungsang <21700383@handong.ac.kr>
2024-05-22 03:31:22 +0900

    [Update] Update RestController

--- src/main/java/egovframework/ocr/sample/web/OcrRestController.java
+++ src/main/java/egovframework/ocr/sample/web/OcrRestController.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
@@ -20,16 +24,21 @@ import javax.servlet.annotation.MultipartConfig;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.aspose.cells.Workbook;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
 import com.kennycason.kumo.CollisionMode;
 import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.WordFrequency;
@@ -39,6 +48,9 @@ import com.kennycason.kumo.palette.ColorPalette;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
+
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
 
 /**
 * jsp파일들의 호출을 처리하는 컨트롤러 클래스
@@ -62,6 +74,9 @@ import com.theokanning.openai.service.OpenAiService;
         maxRequestSize = 1024*1024*5*5
         )
 public class OcrRestController {
+	
+	private int maxInputToken = UseGPT.maxInputToken;
+	private int maxOutputToken = UseGPT.maxOutputToken;
 	
 	@Autowired
     private ServletContext servletContext;
@@ -95,20 +110,27 @@ public class OcrRestController {
         String preprocessingResult = ""; // ChatGPT에게 오타수정을 요청한 후 텍스트
         String summaryText = ""; // 요약 텍스트
         String fileName = file.getOriginalFilename(); // 파일의 이름
+        String afterDetectResult = ""; // 민감정보 검출 후 텍스트
+        
+        fullPath = convertToPdf(fullPath, fileName);
         
         result = OcrTesseract.ocrTess(file.getOriginalFilename(), language, UPLOAD_DIR); 
         
-        language = languageFirst(language);
+        language = languageFirst(language).toUpperCase();
         prompt = "FIX_TYPO_" + language.toUpperCase(); // FIX_TYPO_KOR, FIX_TYPO_ENG
-        preprocessingResult = UseGPT.useGPT(Prompts.getPrompt(prompt), result); // text after using ChatGPT to fix typos
+        preprocessingResult = blockRequest(language, prompt, result, maxOutputToken); // text after using ChatGPT to fix typos
         preprocessingResult = preprocessingResult.replaceAll("\"", ""); // restapi로 호출할때 오류를 일으키는 큰 따옴표 제거
-        summaryText = summary(preprocessingResult, language);
+        prompt = "DETECT_SEN_" + language.toUpperCase(); // DETECT_SEN_KOR, DETECT_SEN_ENG
+        afterDetectResult = blockRequest(language, prompt, preprocessingResult, maxOutputToken);
+        afterDetectResult = afterDetectResult.replaceAll("\"", "");
+        summaryText = summary(afterDetectResult, language);
         // 리턴값으로 돌려줄 파일이름, 언어, 오타수정 결과 텍스트
         Map<String, String> response = new HashMap<>();
         response.put("fileName", fileName);
         response.put("language", language);
         response.put("rawText", result);
         response.put("preprocessingResult", preprocessingResult);
+        response.put("afterDetectResult", afterDetectResult);
         response.put("summary", summaryText);
         
         deleteFile(fullPath);
@@ -136,18 +158,23 @@ public class OcrRestController {
          String prompt = ""; // ChatGPT에게 보낼 명령어
          String result = ""; // 테서렉트를 돌리고 안의 스페이스와 "을 없앤 텍스트
          String preprocessingResult = ""; // ChatGPT에게 오타수정을 요청한 후 텍스트
+         String afterDetectResult = ""; // 민감정보 검출 후 텍스트
          String summaryText = ""; // 요약 텍스트
          String fileName = file.getOriginalFilename(); // 파일의 이름
          int start = 1, end = 1;
          
+         fullPath = convertToPdf(fullPath, fileName);
 
          result = pageSpecific(language, startPage, endPage, fullPath, result, start, end);
          
-         language = languageFirst(language);
-         prompt = "FIX_TYPO_" + language.toUpperCase(); // FIX_TYPO_KOR, FIX_TYPO_ENG
-         preprocessingResult = UseGPT.useGPT(Prompts.getPrompt(prompt), result); // text after using ChatGPT to fix typos
+         language = languageFirst(language).toUpperCase();
+         prompt = "FIX_TYPO_" + language; // FIX_TYPO_KOR, FIX_TYPO_ENG
+         preprocessingResult = blockRequest(language, prompt, result, maxOutputToken); // text after using ChatGPT to fix typos
          preprocessingResult = preprocessingResult.replaceAll("\"", ""); // restapi로 호출할때 오류를 일으키는 큰 따옴표 제거
-         summaryText = summary(preprocessingResult, language);
+         prompt = "DETECT_SEN_" + language; // DETECT_SEN_KOR, DETECT_SEN_ENG
+         afterDetectResult = blockRequest(language, prompt, preprocessingResult, maxOutputToken);
+         afterDetectResult = afterDetectResult.replaceAll("\"", "");
+         summaryText = summary(afterDetectResult, language);
          
          // 리턴값으로 돌려줄 파일이름, 언어, 오타수정 결과 텍스트
          Map<String, String> response = new HashMap<>();
@@ -155,12 +182,38 @@ public class OcrRestController {
          response.put("language", language);
          response.put("rawText", result);
          response.put("preprocessingResult", preprocessingResult);
+         response.put("afterDetectResult", afterDetectResult);
          response.put("summary", summaryText);
          
          deleteFile(fullPath);
          
          return ResponseEntity.ok(response);
      }
+     
+	private String convertToPdf(String fullPath, String fileName) {
+		if (fileName != null && !fileName.isEmpty()) {
+             String extension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
+             
+             if (extension.equals("docx") || extension.equals("doc")) {
+             	fullPath = docToPdf(fullPath);
+             }
+             else if (extension.equals("pptx") || extension.equals("ppt")) {
+             	fullPath = pptToPdf(fullPath);
+             }
+             else if (extension.equals("xlsx") || extension.equals("xls")) {
+             	try {
+ 					fullPath = xslToPdf(fullPath);
+ 				} catch (Exception e) {
+ 					// TODO Auto-generated catch block
+ 					e.printStackTrace();
+ 				}
+             }
+             else if (extension.equals("hwp")) {
+             	
+             }
+         }
+		return fullPath;
+	}
 
      private void deleteFile(String fullPath) {
  		if (fullPath != null) { // remove temporary file
@@ -229,7 +282,7 @@ public class OcrRestController {
         String prompt = "SUMMARY_" + language.toUpperCase(); // SUMMARY_KOR, SUMMARY_ENG등 언어에 맞는 요약 요청 프롬포트
         String summaryText = ""; // 요약 텍스트를 보관
         
-        summaryText = UseGPT.useGPT(Prompts.getPrompt(prompt), text);
+        summaryText = blockRequest(language, prompt, text, maxOutputToken);
         summaryText = summaryText.replaceAll("\\.", ".\n"); // .뒤에 엔터키를 적용"
         summaryText = summaryText.replaceAll("(?m)^[\\s&&[^\\n]]+|^[\n]", ""); // 엔터키로 인해 생긴 스페이스를 지워줌
 
@@ -256,13 +309,13 @@ public class OcrRestController {
      */
      @PostMapping("/tag")
      public ResponseEntity<?> vision(@RequestParam("scanResult") String scanResult, @RequestParam("language") String language) throws IllegalStateException, IOException {
-    	 language = languageFirst(language); // kor+eng와 같은 형태에서 앞의 kor만 사용
+    	 language = languageFirst(language).toUpperCase(); // kor+eng와 같은 형태에서 앞의 kor만 사용
     	 String prompt = "TAG_" + language.toUpperCase(); // TAG_KOR, TAG_ENG등 언어에 맞는 요약 요청 프롬포트
          String jsonTag = ""; // json 형식의 요약 태그를 보관
          String topTags = ""; // 태그들중 가장 빈도수가 높은 태그 5가지
          String purpose = ""; // 태그를 기반으로 한 텍스트의 의도 추출
          
-         jsonTag = getTags(scanResult, prompt);
+         jsonTag = getTags(language, scanResult, prompt);
          topTags = getTopTags(language, jsonTag);
          purpose = getPurpose(scanResult, language, topTags);
          
@@ -293,22 +346,26 @@ public class OcrRestController {
          String prompt = ""; // ChatGPT에게 보낼 명령어
          String result = ""; // 테서렉트를 돌리고 안의 스페이스와 "을 없앤 텍스트
          String preprocessingResult = ""; // ChatGPT에게 오타수정을 요청한 후 텍스트
+         String afterDetectResult = ""; // 민감정보 검출 후 텍스트
          
          result = OcrTesseract.ocrTess(file.getOriginalFilename(), language, UPLOAD_DIR); 
          
-         language = languageFirst(language);  // kor+eng와 같은 형태에서 앞의 kor만 사용
-         prompt = "FIX_TYPO_" + language.toUpperCase(); // FIX_TYPO_KOR, FIX_TYPO_ENG
-         preprocessingResult = UseGPT.useGPT(Prompts.getPrompt(prompt), result); // text after using ChatGPT to fix typos
+         language = languageFirst(language).toUpperCase();  // kor+eng와 같은 형태에서 앞의 kor만 사용
+         prompt = "FIX_TYPO_" + language; // FIX_TYPO_KOR, FIX_TYPO_ENG
+         preprocessingResult = blockRequest(language, prompt, result, maxOutputToken); // text after using ChatGPT to fix typos
          preprocessingResult = preprocessingResult.replaceAll("\"", ""); // restapi로 호출할때 오류를 일으키는 큰 따옴표 제거
-    	 
+         prompt = "DETECT_SEN_" + language; // DETECT_SEN_KOR, DETECT_SEN_ENG
+         afterDetectResult = blockRequest(language, prompt, preprocessingResult, maxOutputToken);
+         afterDetectResult = afterDetectResult.replaceAll("\"", "");
+         
          prompt = "TAG_" + language.toUpperCase(); // TAG_KOR, TAG_ENG등 언어에 맞는 요약 요청 프롬포트
          String jsonTag = ""; // json 형식의 요약 태그를 보관
          String topTags = ""; // 태그들중 가장 빈도수가 높은 태그 5가지
          String purpose = ""; // 태그를 기반으로 한 텍스트의 의도 추출
          
-         jsonTag = getTags(preprocessingResult, prompt);
+         jsonTag = getTags(language, afterDetectResult, prompt);
          topTags = getTopTags(language, jsonTag);
-         purpose = getPurpose(preprocessingResult, language, topTags);
+         purpose = getPurpose(afterDetectResult, language, topTags);
          
          Map<String, String> response = new HashMap<>();
          response.put("jsonTag", jsonTag);
@@ -316,6 +373,7 @@ public class OcrRestController {
          response.put("purpose", purpose);
          response.put("imgLink", convertToLink(jsonTag)); // 태그를 기반으로 만든 wordcloud 이미지의 링크
          response.put("preprocessingResult", preprocessingResult);
+         response.put("afterDetectResult", afterDetectResult);
          
          deleteFile(fullPath);
          
@@ -342,23 +400,27 @@ public class OcrRestController {
          String prompt = ""; // ChatGPT에게 보낼 명령어
          String result = ""; // 테서렉트를 돌리고 안의 스페이스와 "을 없앤 텍스트
          String preprocessingResult = ""; // ChatGPT에게 오타수정을 요청한 후 텍스트
+         String afterDetectResult = ""; // 민감정보 검출 후 텍스트
          int start = 1, end = 1;
          
          result = pageSpecific(language, startPage, endPage, fullPath, result, start, end);
          
-         language = languageFirst(language);  // kor+eng와 같은 형태에서 앞의 kor만 사용
-         prompt = "FIX_TYPO_" + language.toUpperCase(); // FIX_TYPO_KOR, FIX_TYPO_ENG
-         preprocessingResult = UseGPT.useGPT(Prompts.getPrompt(prompt), result); // text after using ChatGPT to fix typos
+         language = languageFirst(language).toUpperCase();  // kor+eng와 같은 형태에서 앞의 kor만 사용
+         prompt = "FIX_TYPO_" + language; // FIX_TYPO_KOR, FIX_TYPO_ENG
+         preprocessingResult = blockRequest(language, prompt, result, maxOutputToken); // text after using ChatGPT to fix typos
          preprocessingResult = preprocessingResult.replaceAll("\"", ""); // restapi로 호출할때 오류를 일으키는 큰 따옴표 제거
-    	 
+         prompt = "DETECT_SEN_" + language; // DETECT_SEN_KOR, DETECT_SEN_ENG
+         afterDetectResult = blockRequest(language, prompt, preprocessingResult, maxOutputToken);
+         afterDetectResult = afterDetectResult.replaceAll("\"", "");
+         
          prompt = "TAG_" + language.toUpperCase(); // TAG_KOR, TAG_ENG등 언어에 맞는 요약 요청 프롬포트
          String jsonTag = ""; // json 형식의 요약 태그를 보관
          String topTags = ""; // 태그들중 가장 빈도수가 높은 태그 5가지
          String purpose = ""; // 태그를 기반으로 한 텍스트의 의도 추출
          
-         jsonTag = getTags(preprocessingResult, prompt);
+         jsonTag = getTags(language, afterDetectResult, prompt);
          topTags = getTopTags(language, jsonTag);
-         purpose = getPurpose(preprocessingResult, language, topTags);
+         purpose = getPurpose(afterDetectResult, language, topTags);
          
          Map<String, String> response = new HashMap<>();
          response.put("jsonTag", jsonTag);
@@ -366,6 +428,7 @@ public class OcrRestController {
          response.put("purpose", purpose);
          response.put("imgLink", convertToLink(jsonTag)); // 태그를 기반으로 만든 wordcloud 이미지의 링크
          response.put("preprocessingResult", preprocessingResult);
+         response.put("afterDetectResult", afterDetectResult);
          
          deleteFile(fullPath);
          
@@ -373,13 +436,13 @@ public class OcrRestController {
      }
      
      private String getPurpose(String scanResult, String language, String topTags) {
- 		String prompt;
+    	String prompt;
  		String purpose;
  		String tagAndText;
  		
  		prompt = "PUR_" + language.toUpperCase();
           tagAndText = topTags + "\n" + scanResult;
-          purpose = UseGPT.useGPT(Prompts.getPrompt(prompt), tagAndText); // topTags 기반으로 텍스트의 의도 추출
+          purpose = blockRequest(language, prompt, tagAndText, maxInputToken); // topTags 기반으로 텍스트의 의도 추출
           
           System.out.println("[rest] prompt: " + prompt);
           System.out.println("[rest] getPrompt: " + Prompts.getPrompt(prompt));
@@ -399,21 +462,52 @@ public class OcrRestController {
           System.out.println("[rest] getPrompt: " + Prompts.getPrompt(prompt));
           System.out.println("[rest] tags: " + jsonTag);
           
-          topTags = UseGPT.useGPT(Prompts.getPrompt(prompt), jsonTag); // 가장 빈도수 높은 태그 5개로 추리기
+          topTags = blockRequest(language, prompt, jsonTag, maxInputToken); // 가장 빈도수 높은 태그 5개로 추리기
  		return topTags;
  	}
  	
- 	private String getTags(String scanResult, String prompt) {
+ 	private String getTags(String language, String scanResult, String prompt) {
  		String jsonTag;
  		
  		System.out.println("[rest] prompt: " + prompt);
           System.out.println("[rest] getPrompt: " + Prompts.getPrompt(prompt));
           System.out.println("[rest] scanResult: " + scanResult);
           
-          jsonTag = UseGPT.useGPT(Prompts.getPrompt(prompt), scanResult);
+          jsonTag = blockRequest(language, prompt, scanResult, maxInputToken);
+          jsonTag = concatJson(jsonTag);
  		return jsonTag;
  	}
      
+ 	private String concatJson(String jsonString) {
+		jsonString = jsonString.replace("}{", ", ");
+		jsonString = jsonString.replace("} {", ", ");
+		return jsonString;
+	}
+ 	
+ 	private String blockRequest(String language, String prompt, String result, int tokenNum) { // 언어, 프롬프트, 결과, 사용 토큰 숫자
+		String blockText, blockOutput; // 각 블력의 텍스트, 호출된 결과를 받기 위한 함수
+		String mergeResult = ""; // 전체 텍스트. 각 블럭당 호출의 결과물을 합침
+		int charNum, blockNum = 0, i, endIndex;
+		
+		charNum = result.length();
+		System.out.println("charNum: " + charNum);
+		blockNum = (charNum/tokenNum) + 1; // 텍스트 글자수 / 전체 입력 토큰의 반올림을 블럭의 갯수로 정함
+		System.out.println("blockNum: " + blockNum);
+		
+		for (i = 0; i < blockNum; i++) { // 블럭 갯수 만큼 반복
+			System.out.println("Start");
+			endIndex = Math.min((i + 1) * tokenNum, charNum); // 인덱스의 끝부분 표시. 최대를 넘지 않도록 비교함
+			blockText = result.substring(i * tokenNum, endIndex); // 현재 인덱스에서 끝 인덱스까지
+			System.out.println("blockText: " + blockText);
+			System.out.println("Promt: " + Prompts.getPrompt(prompt));
+			blockOutput = UseGPT.useGPT(Prompts.getPrompt(prompt), blockText); // 블럭에 대한 요청을 받기
+			System.out.println("End");
+			mergeResult = mergeResult.concat(blockOutput); // GPT 호출 내용 합치기
+		}
+		
+		return mergeResult;
+	}
+ 	
      private static String convertToLink(String jsonString) {
          // JSON 문자열을 중괄호를 기준으로 나누어 배열로 변환
          String[] keyValuePairs = jsonString.substring(1, jsonString.length() - 1).split(",");
@@ -446,4 +540,48 @@ public class OcrRestController {
          return "https://quickchart.io/wordcloud?text=" + queryString.toString() + "&useWordList=true";
      }
     
+     public String docToPdf(String docPath) {
+ 		String pdfPath = null;
+ 	    try {
+ 	        InputStream doc = new FileInputStream(new File(docPath));
+ 	        XWPFDocument document = new XWPFDocument(doc);
+ 	        PdfOptions options = PdfOptions.create();
+ 	        
+ 	        pdfPath = docPath.substring(0, docPath.lastIndexOf('.')) + ".pdf";
+ 	        
+ 	        OutputStream out = new FileOutputStream(new File(pdfPath));
+ 	        PdfConverter.getInstance().convert(document, out, options);
+ 	    } catch (IOException ex) {
+ 	        System.out.println(ex.getMessage());
+ 	    }
+ 	    return pdfPath;
+ 	}
+ 	
+ 	private String xslToPdf(String xslPath) {
+ 		String pdfPath = null;
+ 		Workbook workbook;
+ 		try {
+ 			System.out.println("start");
+ 			workbook = new Workbook(xslPath);
+ 			pdfPath = xslPath.substring(0, xslPath.lastIndexOf('.')) + ".pdf";
+ 			System.out.println("pdfPath : " + pdfPath);
+// 			Worksheet ws = workbook.getWorksheets().get(0);
+// 			Shape sh = ws.getShapes().get(0);
+// 			sh.getFill().getTextureFill().setTiling(true);
+ 			workbook.save(pdfPath);
+ 		} catch (Exception e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+ 		
+ 		return pdfPath;
+ 	}
+ 	private String pptToPdf(String pptPath) {
+ 		String pdfPath = null;
+ 		Presentation presentation = new Presentation(pptPath);
+ 		pdfPath = pptPath.substring(0, pptPath.lastIndexOf('.')) + ".pdf";
+ 		presentation.save(pdfPath, SaveFormat.Pdf);
+ 		
+ 		return pdfPath;
+ 	}
 }
